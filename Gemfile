156ddb9dc9ef6db3d0f7cabd12d92fcb99d85f1c
Maxime Blain <max.b64@orange.fr>
2018-02-16 11:18:35 +0100

    Add figaro - Protect my secret data in config/application.yml

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 ruby '2.4.3'
 
+gem 'figaro'
 gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,22 @@
-Ingredient.destroy_all
+require 'json'
+require 'open-uri'
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+puts "Deleting all the ingredients & cocktails\o/"
+Cocktail.destroy_all
+Ingredient.destroy_all
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+list = JSON.parse(user_serialized)
+list["drinks"].each do |drink|
+  Ingredient.create!(name: drink["strIngredient1"])
+end
+url2 = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
+user_serialized2 = open(url2).read
+list2 = JSON.parse(user_serialized2)
+list2["drinks"].each do |drink|
+  Cocktail.create!(
+    name: drink["strDrink"],
+    picture: "https://#{drink["strDrinkThumb"]}"
+    )
+end
+puts "the end"
